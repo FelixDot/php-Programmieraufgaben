a923bd0501531babcfa0e493a973ecf1953607c0
FelixDot <felix.tran002@gmail.com>
2024-04-15 11:37:01 +0200

    collect all unique mac addresses from a serial number, count the mac addresses, select the top 10 serial numbers with the most mac addresses

---
+++ task2.php
@@ -0,0 +1,49 @@
+<?php
+$logFile = './updatev12-access-pseudonymized.log';
+$fileOpen = fopen($logFile, 'r');
+$serialMacMap = [];
+
+if ($fileOpen) {
+    while (!feof($fileOpen)) {
+        $line = fgets($fileOpen);
+
+        $serialPos = strpos($line, 'serial=');
+        $specsPos = strpos($line, 'specs=');
+
+        $serialPart = substr($line, $serialPos + 7);
+        $serial = trim(substr($serialPart, 0, strpos($serialPart, ' ')));
+        $specsPart = substr($line, $specsPos + 6);
+        $specs = trim(substr($specsPart, 0, strpos($specsPart, ' ')));
+
+        // Decode and decompress specs
+        $decodedSpecs = base64_decode($specs);
+        $decompressedSpecs = gzdecode($decodedSpecs);
+
+        // Decode json object
+        $data = json_decode($decompressedSpecs);
+        
+        // Get mac address
+        $macAddress = $data->mac;
+
+        // Store per serial each unique mac address
+        if (!isset($serialMacMap[$serial])) {
+            $serialMacMap[$serial] = [];
+        }
+        if (!in_array($macAddress, $serialMacMap[$serial])) {
+            $serialMacMap[$serial][] = $macAddress;
+        }
+    }
+    fclose($fileOpen);
+    $serialCounts = [];
+    
+    // Count per serial the number of mac addresses
+    foreach ($serialMacMap as $serial => $macAddresses) {
+        $serialCounts[$serial] = count($macAddresses);
+    }
+    arsort($serialCounts);
+    $topSerials = array_slice(array_keys($serialCounts), 0, 10);
+    foreach ($topSerials as $serial) {
+        echo "Serialnumber: $serial, number of devices installed on $serialCounts[$serial] <br>";
+    }
+}
+?>
